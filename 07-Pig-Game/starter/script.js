c46c5ed0a877f361c7f53f4689eeb43d72469f53
Gabriel <2021240@student.cct.ie>
2023-02-03 23:44:01 +0000

    Switching active player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,9 @@
 "use strict";
 //DOM element
 //Selecting elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+
 const score0EL = document.querySelector("#score--0");
 const score1EL = document.getElementById("score--1");
 const current0El = document.getElementById("current--0");
@@ -15,7 +18,10 @@ const btnHold = document.querySelector(".btn--hold");
 score0EL.textContent = 0;
 score1EL.textContent = 0;
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
 //hidde element dice
 diceEl.classList.add("hidden");
 
@@ -33,8 +39,15 @@ btnRoll.addEventListener("click", function() {
     if (dice !== 1) {
         //Add dice to current score
         currentScore += dice;
-        current0El.textContent = currentScore; //CHANGE LATER
+        document.getElementById(`current--${activePlayer}`).textContent =
+            currentScore;
+        //current0El.textContent = currentScore; //CHANGE LATER
     } else {
         //Switch to next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle("player--active");
+        player1El.classList.toggle("player--active");
     }
 });
